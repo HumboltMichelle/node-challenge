db0eda63958bfacb2da0b988d5eada4798f84d04
dwayneRichards <dwaynerichards@gmail.com>
2021-06-18 15:18:53 -0400

    made it to before servinf files

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const server = http.createServer(doOnRequest);
+server.listen(3000);
+const chalk = require('chalk');
+const { resolve } = require('path');
+const warning = chalk.redBright
 
-function doOnRequest(request, response){
-  console.log(request)
+
+function doOnRequest(request, response) {
+  console.log(warning('************************* new request ****************'))
+  //console.log(request)
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
+  //response.end("yo")  
   if (request.method === 'GET' && request.url === '/') {
     // read the index.html file and send it back to the client
     // code here...
@@ -18,7 +25,23 @@ function doOnRequest(request, response){
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
     // code here...
-    
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+
+    });
+    request.on('end', () => {
+      console.log(warning('Body b4 Buffer.concat'), body);
+
+      body = Buffer.concat(body).toString();
+      
+      console.log(warning(`this is the body after buffer`), body);
+      (body === 'hello') ? response.end('hello there!')
+        : (body === 'whats up') ? response.end('The sky')
+        : response.end('good morning')
+    }
+
+    )
   }
   else {
     // Handle 404 error: page not found
